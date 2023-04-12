aaece5dc8d3b776a7814f44e232456d38a99d1df
Jumpyyy <1358080420@qq.com>
2023-04-12 14:15:10 +0800

    cut password

--- app/secure.py
+++ app/secure.py
@@ -4,15 +4,15 @@
 __author__ = 'ldd'
 
 DEBUG = True
-SQLALCHEMY_DATABASE_URI = 'mysql+cymysql://root:123456@localhost:3306/fisher'
-SECRET_KEY = '125ERDFDS5656EW15RSFDD15354645212RW'
+SQLALCHEMY_DATABASE_URI = 'mysql+cymysql://root:xxx@localhost:3306/fisher'
+SECRET_KEY = 'xxxxxxxxxxxxxxxxxxxxxx'
 
 # Email 配置
 MAIL_SERVER = 'smtp.qq.com'
 MAIL_PORT = 465
 MAIL_USE_SSL = True
 MAIL_USE_TSL = False
-MAIL_USERNAME = '1358080420@qq.com'
-MAIL_PASSWORD = 'nwvlxbaihfrtfgdg'
+MAIL_USERNAME = 'xxx@qq.com'
+MAIL_PASSWORD = 'xxxxxx'
 # MAIL_SUBJECT_PREFIX = '[鱼书]'
 # MAIL_SENDER = '鱼书 <hello@yushu.im> hello ldd'
